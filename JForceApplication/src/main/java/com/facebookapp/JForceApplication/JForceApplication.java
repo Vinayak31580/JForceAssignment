a2781a3c0eb1b78a3f40936740b387cea71ec126
Vinayak31580 <63110052+Vinayak31580@users.noreply.github.com>
2021-12-08 08:33:31 +0530

    Create JForceApplication.java

---
+++ JForceApplication/src/main/java/com/facebookapp/JForceApplication/JForceApplication.java
@@ -0,0 +1,13 @@
+package com.facebokapp.JForceApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JForceApplication.class, args);
+	}
+
+}
